c9b7c9c9d02cc338f8bbcfe3bdeb85fcdfbbaa4c
flonnqvi <fredrik.lonnqvist@crosskey.fi>
2023-11-29 15:12:57 +0200

    lazy gitlinker

--- lua/flonka/plugins/git.lua
+++ lua/flonka/plugins/git.lua
@@ -45,13 +45,49 @@ return {
       end,
     },
   },
-  -- remote link
   {
     "ruifm/gitlinker.nvim",
     version = "*",
     dependencies = {
       "nvim-lua/plenary.nvim",
     },
-    opts = {},
+    lazy = true,
+    event = { "BufReadPre", "BufNewFile" }, -- to disable, comment this out
+    opts = {
+      callbacks = {
+        -- Bitbucket onpremise server
+        ["bitbucket.crosskey.fi"] = function(url_data)
+          -- Create a table to store the split parts
+          local repoSplit = {}
+          -- Use string.gmatch to iterate over the string parts
+          for part in url_data.repo:gmatch "[^/]+" do
+            table.insert(repoSplit, part)
+          end
+
+          local url = "https://" .. url_data.host
+          if url_data.port then
+            url = url .. ":" .. url_data.port
+          end
+
+          url = url
+            .. "/projects/"
+            .. repoSplit[1]
+            .. "/repos/"
+            .. repoSplit[2]
+            .. "/browse/"
+            .. url_data.file
+            .. "?at="
+            .. url_data.rev
+
+          if url_data.lstart then
+            url = url .. "#" .. url_data.lstart
+            if url_data.lend then
+              url = url .. "-" .. url_data.lend
+            end
+          end
+          return url
+        end,
+      },
+    },
   },
 }
