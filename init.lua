9f0034a3d6868d1619199f7831c6252c99cc3892
flonnqvi <fredrik.lonnqvist@crosskey.fi>
2023-11-29 14:26:35 +0200

    clean up

--- init.lua
+++ init.lua
@@ -16,7 +16,17 @@ require("gitlinker").setup {
       if url_data.port then
         url = url .. ":" .. url_data.port
       end
-      url = url .. "/projects/" .. repoSplit[1] .. "/repos/" .. repoSplit[2] .. "/browse/" .. url_data.file .. "?at=" .. url_data.rev
+
+      url = url
+        .. "/projects/"
+        .. repoSplit[1]
+        .. "/repos/"
+        .. repoSplit[2]
+        .. "/browse/"
+        .. url_data.file
+        .. "?at="
+        .. url_data.rev
+
       if url_data.lstart then
         url = url .. "#" .. url_data.lstart
         if url_data.lend then
@@ -27,23 +37,6 @@ require("gitlinker").setup {
     end,
   },
 }
-
--- local url = M.get_base_https_url(url_data)
---   if not url_data.file or not url_data.rev then
---     return url
---   end
---   url = url .. "/src/" .. url_data.rev .. "/" .. url_data.file
---
---   if not url_data.lstart then
---     return url
---   end
---   url = url .. "#lines-" .. url_data.lstart
---   if url_data.lend then
---     url = url .. ":" .. url_data.lend
---   end
---
---   return url
--- end
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
@@ -230,51 +223,6 @@ vim.defer_fn(function()
   }
 end, 0)
 
--- [[ Configure LSP ]]
---  This function gets run when an LSP connects to a particular buffer.
-local on_attach = function(_, bufnr)
-  -- NOTE: Remember that lua is a real programming language, and as such it is possible
-  -- to define small helper and utility functions so you don't have to repeat yourself
-  -- many times.
-  --
-  -- In this case, we create a function that lets us more easily define mappings specific
-  -- for LSP related items. It sets the mode, buffer and description for us each time.
-  local nmap = function(keys, func, desc)
-    if desc then
-      desc = "LSP: " .. desc
-    end
-
-    vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
-  end
-
-  nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-  nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-
-  nmap("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-  nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-  nmap("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
-  nmap("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
-  nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-  nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
-
-  -- See `:help K` for why this keymap
-  nmap("K", vim.lsp.buf.hover, "Hover Documentation")
-  nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
-
-  -- Lesser used LSP functionality
-  nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-  nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
-  nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
-  nmap("<leader>wl", function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, "[W]orkspace [L]ist Folders")
-
-  -- Create a command `:Format` local to the LSP buffer
-  vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
-    vim.lsp.buf.format()
-  end, { desc = "Format current buffer with LSP" })
-end
-
 -- document existing key chains
 require("which-key").register {
   ["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
@@ -344,7 +292,6 @@ mason_lspconfig.setup_handlers {
   function(server_name)
     require("lspconfig")[server_name].setup {
       capabilities = capabilities,
-      -- on_attach = on_attach,
       settings = servers[server_name],
       filetypes = (servers[server_name] or {}).filetypes,
     }
