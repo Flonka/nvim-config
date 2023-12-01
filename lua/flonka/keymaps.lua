9908e7dc29e11194af32b96f23fcd277b3ec3b7b
flonnqvi <fredrik.lonnqvist@crosskey.fi>
2023-12-01 15:24:15 +0200

    simplify nvimtree keybinds, only using the toggle.

--- lua/flonka/keymaps.lua
+++ lua/flonka/keymaps.lua
@@ -22,7 +22,7 @@ vim.keymap.set("n", "<leader>ll", vim.diagnostic.open_float, { desc = "Open floa
 vim.keymap.set("n", "<leader>lL", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
 
 -- [[ nvim tree keymaps ]]
-vim.keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
-vim.keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
-vim.keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
-vim.keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
+vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
+-- vim.keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
+-- vim.keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
+-- vim.keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
