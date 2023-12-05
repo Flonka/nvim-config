4183eb1ff75494653e65cdad48a44fbbb334b154
flonnqvi <fredrik.lonnqvist@crosskey.fi>
2023-12-05 15:42:34 +0200

    move gui related plugins to own module

--- lua/flonka/lazy.lua
+++ lua/flonka/lazy.lua
@@ -23,42 +23,6 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   { import = "flonka.plugins" },
 
-  -- Useful plugin to show you pending keybinds.
-  { "folke/which-key.nvim", event = "VeryLazy", opts = {} },
-
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme "catppuccin"
-    end,
-  },
-
-  {
-    -- Set lualine as statusline
-    "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
-    -- See `:help lualine.txt`
-    opts = {
-      options = {
-        -- icons_enabled = false,
-        theme = "catppuccin",
-        -- component_separators = '|',
-        -- section_separators = '',
-      },
-    },
-  },
-
-  {
-    -- Add indentation guides even on blank lines
-    "lukas-reineke/indent-blankline.nvim",
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help ibl`
-    main = "ibl",
-    opts = {},
-  },
-
   -- "gc" to comment visual regions/lines
   { "numToStr/Comment.nvim", event = "BufReadPost", opts = {} },
 
---
+++ lua/flonka/plugins/gui.lua
@@ -0,0 +1,73 @@
+return {
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      routes = {
+        {
+          view = "notify",
+          filter = { event = "msg_showmode" },
+        },
+      },
+      lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+      },
+      -- you can enable a preset for easier configuration
+      presets = {
+        bottom_search = true, -- use a classic bottom cmdline for search
+        command_palette = true, -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = false, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = false, -- add a border to hover docs and signature help
+      },
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      -- "rcarriga/nvim-notify",
+    },
+  },
+  -- Useful plugin to show you pending keybinds.
+  { "folke/which-key.nvim", event = "VeryLazy", opts = {} },
+
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme "catppuccin"
+    end,
+  },
+
+  {
+    -- Set lualine as statusline
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    -- See `:help lualine.txt`
+    opts = {
+      options = {
+        -- icons_enabled = false,
+        theme = "catppuccin",
+        -- component_separators = '|',
+        -- section_separators = '',
+      },
+    },
+  },
+
+  {
+    -- Add indentation guides even on blank lines
+    "lukas-reineke/indent-blankline.nvim",
+    -- Enable `lukas-reineke/indent-blankline.nvim`
+    -- See `:help ibl`
+    main = "ibl",
+    opts = {},
+  },
+}
