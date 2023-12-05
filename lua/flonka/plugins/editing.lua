22acdaff106972d52df0c4c3945d616d3ec49140
flonnqvi <fredrik.lonnqvist@crosskey.fi>
2023-12-05 16:08:01 +0200

    Move rest from lazy module to specifics

--- lua/flonka/plugins/editing.lua
+++ lua/flonka/plugins/editing.lua
@@ -9,6 +9,7 @@ return {
       "nvim-treesitter/nvim-treesitter-textobjects",
     },
     config = function()
+      ---@diagnostic disable-next-line missing-fields
       require("nvim-surround").setup {
         -- Configuration here, or leave empty to use defaults
       }
@@ -19,5 +20,51 @@ return {
   {
     "tpope/vim-sleuth",
     event = { "BufReadPost", "BuFNewFile" },
+  },
+
+  -- "gc" to comment visual regions/lines
+  { "numToStr/Comment.nvim", event = { "BufReadPost", "BuFNewFile" }, opts = {} },
+
+  {
+    "stevearc/conform.nvim",
+    event = { "BufReadPre", "BufNewFile" }, -- to disable, comment this out
+    config = function()
+      local conform = require "conform"
+
+      local format_options = {
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 5000,
+      }
+
+      conform.setup {
+        formatters_by_ft = {
+          javascript = { "prettier" },
+          css = { "prettier" },
+          html = { "prettier" },
+          json = { "prettier" },
+          yaml = { "prettier" },
+          markdown = { "prettier" },
+          graphql = { "prettier" },
+          lua = { "stylua" },
+          go = { "goimports", "gofumpt" },
+          -- python = { "isort", "black" },
+        },
+        format_on_save = format_options,
+      }
+
+      vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+        conform.format(format_options)
+      end, { desc = "Format file or range (in visual mode)" })
+    end,
+  },
+  {
+    -- Highlight, edit, and navigate code
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    build = ":TSUpdate",
   },
 }
---
+++ lua/flonka/plugins/navigation.lua
@@ -0,0 +1,61 @@
+return {
+  -- Fuzzy Finder (files, lsp, etc)
+  {
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      -- Fuzzy Finder Algorithm which requires local dependencies to be built.
+      -- Only load if `make` is available. Make sure you have the system
+      -- requirements installed.
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        -- NOTE: If you are having trouble with this installation,
+        --       refer to the README for telescope-fzf-native for more instructions.
+        build = "make",
+        cond = function()
+          return vim.fn.executable "make" == 1
+        end,
+      },
+    },
+  },
+
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      update_focused_file = {
+        enable = true,
+      },
+      view = {
+        float = {
+          enable = true,
+          open_win_config = function()
+            local HEIGHT_RATIO = 0.8
+            local WIDTH_RATIO = 0.7
+            local screen_w = vim.opt.columns:get()
+            local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+            local window_w = screen_w * WIDTH_RATIO
+            local window_h = screen_h * HEIGHT_RATIO
+            local window_w_int = math.floor(window_w)
+            local window_h_int = math.floor(window_h)
+            local center_x = (screen_w - window_w) / 2
+            local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+            return {
+              border = "rounded",
+              relative = "editor",
+              row = center_y,
+              col = center_x,
+              width = window_w_int,
+              height = window_h_int,
+            }
+          end,
+        },
+      },
+    },
+  },
+}
